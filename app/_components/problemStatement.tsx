6e471656869c70ce4d0e4e91122123d18358eec1
navaneethku <navaneethku1729@gmail.com>
2024-02-22 14:42:11 +0530

    add rounds

--- app/_components/problemStatement.tsx
+++ app/_components/problemStatement.tsx
@@ -1,39 +1,33 @@
 import { CustomText } from "@/components/custom";
 
-const ProblemStatement = () => {
-  const problemStatements = [
+const Round = () => {
+  const rounds = [
     {
-      qn: "Sustainable Construction Materials",
-      ans: "Join us in revolutionising the construction industry towards sustainability! We're seeking innovative solutions to curb carbon emissions and reduce environmental footprints associated with construction materials. Dive into the realm of recycled materials, carbon capture or sequestration, and material alternatives to address challenges while fostering environmental stewardship. ",
+      qn: "Ideation",
+      ans: "Participating teams are given problem statements fairly. Develop an innovative and efficient solution and create a presentation for the same and present it to expert judging panel. The authenticity and scope of the ideas will be evaluated, along with the quality and effectiveness of the solutions presented",
+      date: "February 28",
     },
     {
-      qn: "Structural Stability and Performance in Adverse Conditions ",
-      ans: "Calling all problem solvers! In regions prone to natural calamities and affected by climate change, we're seeking resilient solutions. Join us in reinforcing structures with adaptable designs and materials. Let's implement redundancy and align policies to ensure safer, sustainable built environments resilient to adverse conditions. ",
+      qn: "Prototyping and Pitching",
+      ans: "The shortlisted teams from Round 1 will begin the prototyping session. During this round the teams are to create a prototype of their solution and then do a Business pitch to the judging panel. Money management, Investment, Marketing strategies and Mind map of a team will be evaluated here.",
+      date: "February 29",
     },
     {
-      qn: "Structural Health Monitoring Techniques ",
-      ans: "Be a part of the safety revolution! We're on the lookout for advanced techniques to enhance structural monitoring. Join us in leveraging sensing technologies and data analytics to ensure safer, resilient civil infrastructure. Early detection is key, and your innovative solutions can make a difference! ",
-    },
-    {
-      qn: "Town Planning and Traffic Management with Artificial Intelligence",
-      ans: "Attention innovators! Rapid urbanisation demands cutting-edge solutions. Dive into the world of artificial intelligence and revolutionise town planning and traffic management. Let's optimise resource utilisation, enhance traffic flow, and foster urban sustainability together. Your ideas can shape the cities of tomorrow! ",
-    },
-    {
-      qn: "Microplastics Pollution: Sources, Impacts, and Remediation Strategies",
-      ans: "Join the fight against microplastics pollution! Together, we can make a difference. Explore the sources, understand the impacts, and develop robust mitigation strategies. From improved waste management to innovative cleanup technologies, your solutions are crucial. Let's work hand in hand to protect our planet for future generations!",
+      qn: "Marketing",
+      ans: "Round 3 of the Belfort of Wall Street 2024 , will be conducted in an open area mode at Christ college of Engineering campus. All the selected teams have to be present at the campus where the audience will be your customer. Participants will have to interact with the audience and market their ideas/products. The winner of this round will be decided by audience poll.",
+      date: "February 29",
     },
   ];
   return (
     <div className="z-10">
       <CustomText highlightedTitle className="mb-6">
-        Problem Statements
+        Rounds
       </CustomText>
-      {problemStatements.map((statement, index) => (
+      {rounds.map((statement, index) => (
         <div key={index} className="flex flex-col gap-4 pt-10">
           <div className="flex flex-col gap-1 font-secondary text-2xl md:text-3xl">
             <span className="font-tertiary text-xl text-secondary md:text-2xl">
-              Problem{" "}
-              <span className="whitespace-nowrap">Statement {index + 1}:</span>
+              <span className="whitespace-nowrap">{statement.date}</span>
             </span>
             <span>{statement.qn}</span>
           </div>
@@ -46,4 +40,4 @@ const ProblemStatement = () => {
   );
 };
 
-export default ProblemStatement;
+export default Round;
